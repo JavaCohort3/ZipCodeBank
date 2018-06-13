c78cebf1f0d90a9af964faedb3a82b26631f4dd4
iVesperX <aermias@live.com>
2018-06-13 17:22:14 -0400

    initial commit

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/ZipCodeBankApplication.java
@@ -0,0 +1,12 @@
+package com.example.javacohort3.ZipCodeBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipCodeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipCodeBankApplication.class, args);
+	}
+}
