23d2458c4321ce3dd1cada82fbac5bacfc1a8dd0
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-13 22:56:04 -0400

    added wrote code to Create an Account object

--- src/main/java/com/example/javacohort3/ZipCodeBank/service/AccountService.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.example.javacohort3.ZipCodeBank.service;
 
+import com.example.javacohort3.ZipCodeBank.Account;
 import com.example.javacohort3.ZipCodeBank.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,8 @@ import org.springframework.stereotype.Service;
 public class AccountService {
     @Autowired
     AccountRepository accountRepository;
+
+    public Account createAccount(Account account){
+        return account;
+    }
 }
