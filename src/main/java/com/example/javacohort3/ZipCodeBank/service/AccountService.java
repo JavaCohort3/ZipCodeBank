dcd2db1206053ddc64b32eea598ed73796ae5480
iVesperX <aermias@live.com>
2018-06-13 22:47:23 -0400

    added getCustomerById method in AccountService.java

--- src/main/java/com/example/javacohort3/ZipCodeBank/service/AccountService.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/service/AccountService.java
@@ -1,6 +1,8 @@
 package com.example.javacohort3.ZipCodeBank.service;
 
+import com.example.javacohort3.ZipCodeBank.Customer;
 import com.example.javacohort3.ZipCodeBank.repositories.AccountRepository;
+import com.example.javacohort3.ZipCodeBank.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,11 @@ import org.springframework.stereotype.Service;
 public class AccountService {
     @Autowired
     AccountRepository accountRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public Customer getCustomerById(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
 }
