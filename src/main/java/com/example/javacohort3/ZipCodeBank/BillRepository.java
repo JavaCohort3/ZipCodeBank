61891fdb22f0e0a93d6006278914659df7c0cee0
imowbray97 <isaiah.mowbray97@gmail.com>
2018-06-14 16:39:48 -0400

    Touch Up & Impplemented logic for bill service

--- src/main/java/com/example/javacohort3/ZipCodeBank/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.javacohort3.ZipCodeBank;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BillRepository extends CrudRepository<Bill, Long> {
-    
-}
