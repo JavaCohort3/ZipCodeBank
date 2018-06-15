9f0ca2fdefd632fd2ed0759890a14324e63e4dd8
senorcl0ud <claudemcalpin@gmail.com>
2018-06-15 08:52:10 -0400

    refactor point

--- src/main/java/com/example/javacohort3/ZipCodeBank/controllers/BillController.java
+++
@@ -1,4 +0,0 @@
-package com.example.javacohort3.ZipCodeBank.controllers;
-
-public class BillController {
-}
--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.javacohort3.ZipCodeBank.repositories;
-
-import com.example.javacohort3.ZipCodeBank.domains.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer,Long> {
-}
