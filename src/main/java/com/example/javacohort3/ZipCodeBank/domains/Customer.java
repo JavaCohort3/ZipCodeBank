30fcff71849155177598e0451bbd4591fc15abf7
NightShadow3000 <RoyalFotoK@gmail.com>
2018-06-15 13:45:59 -0400

    The Customer POJO - JoJo

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/domains/Customer.java
@@ -0,0 +1,63 @@
+package com.example.javacohort3.ZipCodeBank.domains;
+
+import java.util.LinkedHashSet;
+
+public class Customer {
+
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private LinkedHashSetSet<Address> address;
+
+    public Customer() { }
+
+    public Customer(Long id, String first_name, String last_name, LinkedHashSet<Address> address) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public LinkedHashSetSet<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(LinkedHashSetSet<Address> address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+}
