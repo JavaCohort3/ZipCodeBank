001d306223394ce30e56bd3c909651208f8060c6
zeeman0306 <zeeman0306@gmail.com>
2018-06-15 13:45:02 -0400

    First commit, added WithdrawalRepository Interface

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/WithdrawalRepository.java
@@ -0,0 +1,10 @@
+package com.example.javacohort3.ZipCodeBank.repositories;
+
+import com.example.javacohort3.ZipCodeBank.domains.Withdrawal;
+import org.springframework.data.repository.CrudRepository;
+
+public interface WithdrawalRepository extends CrudRepository<Withdrawal, Long> {
+
+
+
+}
