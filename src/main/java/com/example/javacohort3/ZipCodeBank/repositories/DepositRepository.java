d60dff4debb15329ca0c52fe34c9131b1f7b4498
iVesperX <aermias@live.com>
2018-06-16 02:43:03 -0400

    updated changes

--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/DepositRepository.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/DepositRepository.java
@@ -3,6 +3,14 @@ package com.example.javacohort3.ZipCodeBank.repositories;
 import com.example.javacohort3.ZipCodeBank.domains.Deposit;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface DepositRepository extends CrudRepository<Deposit, Long> {
     // not sure of the methods
+
+    Deposit createDepositByAccountId(Deposit deposit);
+    Deposit GetDepositById(Long depositId);
+    ArrayList<Deposit> getAllDepositsForAccountId(Long accountId);
+    Deposit updateDeposit(Deposit deposit, Long accountId);
+    void deleteDeposit(Long depositId);
 }
