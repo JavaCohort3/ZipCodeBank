326faa9bc69184b2d801fdfa4b1ea677940d9efc
NightShadow3000 <RoyalFotoK@gmail.com>
2018-06-16 09:38:51 -0400

    Here is my CustomerRepo

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/controller/CustomerController.java
@@ -0,0 +1,4 @@
+package com.example.javacohort3.ZipCodeBank.controller;
+
+public class CustomerController {
+}
---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/CustomerRepository.java
@@ -0,0 +1,18 @@
+package com.example.javacohort3.ZipCodeBank.repositories;
+
+import com.example.javacohort3.ZipCodeBank.domains.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+// all except verify
+
+    Customer createCustomer(Customer customer);
+    Customer getCustomerByAccountId(Long id);
+    ArrayList<Customer> getAllCustomers();
+    Customer getCustomerById(Long id);
+    Customer updateCustomer(Customer customer);
+    void deleteCustomer(Long id);
+
+}
