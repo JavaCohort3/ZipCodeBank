8f7d0e0aee9ebb3a920cf87ef4e62226914746aa
iVesperX <aermias@live.com>
2018-06-15 22:42:35 -0400

    added repositories/DepositRepository.java

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/DepositRepository.java
@@ -0,0 +1,8 @@
+package com.example.javacohort3.ZipCodeBank.repositories;
+
+import com.example.javacohort3.ZipCodeBank.domains.Deposit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DepositRepository extends CrudRepository<Deposit, Long> {
+    // not sure of the methods
+}
