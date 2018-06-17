4b14703edb89fa22c679ebe10124c3d2eff48344
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-16 23:03:47 -0400

    added CrudeRepository

--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/AccountRepository.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/AccountRepository.java
@@ -1,10 +1,11 @@
 package com.example.javacohort3.ZipCodeBank.repositories;
 
 import com.example.javacohort3.ZipCodeBank.domains.Account;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
-public interface AccountRepository {
+public interface AccountRepository extends CrudRepository<Account, Long> {
     Account createAccountFromCustomerId(Account account, Long customerId);
     ArrayList<Account> getAccounsByCustomerId(Long customerId);
     ArrayList<Account> getAllAccounts();
