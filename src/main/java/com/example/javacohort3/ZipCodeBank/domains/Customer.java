1f255a0e5e685dceb25f303e3f7f32c185cc4d88
NightShadow3000 <RoyalFotoK@gmail.com>
2018-06-17 13:15:51 -0400

    Heres the new Customer POJO

--- src/main/java/com/example/javacohort3/ZipCodeBank/domains/Customer.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/domains/Customer.java
@@ -3,14 +3,16 @@ package com.example.javacohort3.ZipCodeBank.domains;
 public class Customer {
 
     private Long id;
+    private Long accountId;
     private String first_name;
     private String last_name;
     private Address address;
 
     public Customer() { }
 
-    public Customer(Long id, String first_name, String last_name, Address address) {
+    public Customer(Long id, Long accountId, String first_name, String last_name, Address address) {
         this.id = id;
+        this.accountId = accountId;
         this.first_name = first_name;
         this.last_name = last_name;
         this.address = address;
@@ -22,6 +24,14 @@ public class Customer {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
     }
 
     public String getFirst_name() {
@@ -52,10 +62,10 @@ public class Customer {
     public String toString() {
         return "Customer{" +
                 "id=" + id +
+                ", accountId=" + accountId +
                 ", first_name='" + first_name + '\'' +
                 ", last_name='" + last_name + '\'' +
                 ", address=" + address +
                 '}';
     }
-
 }
