ae9256982163411f536599cb1d8c9c9e40d84d14
imowbray97 <isaiah.mowbray97@gmail.com>
2018-06-17 10:14:54 -0400

    Fix up The BillService and Also Finshed BillController

--- src/main/java/com/example/javacohort3/ZipCodeBank/controllers/BillController.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/controllers/BillController.java
@@ -1,18 +1,88 @@
 package com.example.javacohort3.ZipCodeBank.controllers;
 
 
+import com.example.javacohort3.ZipCodeBank.domains.Bill;
 import com.example.javacohort3.ZipCodeBank.services.BillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@Controller
+import java.net.URI;
+
+@RestController
 public class BillController {
-    @Autowired
-    BillService billService;
+    private static final Logger log = LoggerFactory.getLogger(SpringApplication.class);
+    public BillService billService;
 
-    @RequestMapping(value = "/accounts/{accountId}/bills")
-    ResponseEntity<?> getAllBillByAccountId()
+    @Autowired
+    public BillController(BillService billService) {
+        this.billService = billService;
+    }
+
+    @RequestMapping(value = "/accounts/{accountId}/bills",method = RequestMethod.GET)
+    public ResponseEntity<?> getBillByAccountId(@PathVariable Long id) {
+        return new ResponseEntity<>(billService.getAllBillByAccountId(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/customers/{customerId}/bills",method = RequestMethod.GET)
+    public ResponseEntity<?> getBillByCustomerId(@PathVariable Long id) {
+        return new ResponseEntity<>(billService.getBillsByCustomerId(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/bills/{billId}", method = RequestMethod.GET)
+    public ResponseEntity<?> getBillById(@PathVariable Long id){
+        return new ResponseEntity<>(billService.getBillById(id),HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/accounts/{accountId}/bills",method = RequestMethod.POST)
+    public ResponseEntity<?> createBill (@RequestBody Bill bill,@PathVariable Long id){
+        bill = billService.createBill(bill,id);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        URI newUri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{accountId}/bills")
+                .buildAndExpand(bill.getId())
+                .toUri();
+
+        httpHeaders.setLocation(newUri);
+
+        return new ResponseEntity<>(bill,httpHeaders,HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/bills/{billId}",method = RequestMethod.PUT)
+    public ResponseEntity<?> updateBill(@RequestBody Bill bill, @PathVariable Long id){
+        HttpStatus status;
+        Bill oldBill = billService.getBillById(id);
+
+        billService.updateBill(bill);
+
+        if (oldBill != null){
+            log.info("Bill consist of  : ",oldBill);
+            status = HttpStatus.OK;
+        }else {
+            log.info("Bill didn't exist so created");
+            status = HttpStatus.CREATED;
+        }
+
+        return new ResponseEntity<>(bill,status);
+    }
+
+    @RequestMapping(value = "/bills/{billId}",method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteBill (@PathVariable Long id){
+        HttpStatus status;
+
+        billService.deleteBill(id);
+        status = HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(status);
+    }
+
 }
--- src/main/java/com/example/javacohort3/ZipCodeBank/services/BillService.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/services/BillService.java
@@ -15,61 +15,63 @@ public class BillService {
 
     @Autowired
     private BillRepository billRepository;
-    @Autowired
-    private AccountRepository accountRepository;
-    @Autowired
-    private CustomerRepository customerRepository;
 
+    public BillService() {
+    }
 
-    void verifyByBillId(Long id){
+    public BillService(BillRepository billRepository) {
+        this.billRepository = billRepository;
+    }
+
+    public void verifyByBillId(Long id){
      if (billRepository.findById(id).orElse(null) == null){
          //throw new ResourceNotFoundException;
      }
     }
 
 
-    Bill createBillByAccountId(Bill bill, Long id){return billRepository.save(bill); }
+    public Bill createBill(Bill bill, Long id){return billRepository.save(bill); }
 
 
-    Bill getBillById(Long id){
+    public Bill getBillById(Long id){
         return billRepository.findById(id).orElse(null);
     }
 
 
-    ArrayList<Bill> getAllBillByAccountId(Long id){
+    public ArrayList<Bill> getAllBillByAccountId(Long id){
         ArrayList<Bill> listOfAllBill = (ArrayList<Bill>) billRepository.findAll();
         for (Bill b : listOfAllBill)
         {
-            if (billRepository.getBillById(id).getAccount_id().equals(b.getId())){
-                billRepository.findById(id);
+            if (b.getAccount_id().equals(id)){
+                billRepository.findById(b.getId());
             }else {
-                throw new ResourceNotFoundException();
+                //throw new ResourceNotFoundException();
             }
         }
         return null;
     }
 
 
-    ArrayList<Bill> getBillsByCustomerId(Long id, Customer customer){
+    public ArrayList<Bill> getBillsByCustomerId(Long id){
         ArrayList<Bill> listOfAllBill = (ArrayList<Bill>) billRepository.findAll();
         for (Bill b : listOfAllBill)
         {
-            if (customer.getId().equals(b.getId())){
-                billRepository.findById(id);
+            if (b.getId().equals(id)){
+                billRepository.findById(b.getId());
             }else {
-                throw new ResourceNotFoundException();
+               // throw new ResourceNotFoundException();
             }
         }
         return null;
     }
 
 
-    Bill updateBill(Bill bill){
+    public Bill updateBill(Bill bill){
         return billRepository.save(bill);
     }
 
 
-    void deleteBill(Long id){
+    public void deleteBill(Long id){
         billRepository.deleteById(id);
     }
 
