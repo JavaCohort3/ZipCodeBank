b5ca32b5ef3a2203ed08114f2ace683adb723974
senorcl0ud <claudemcalpin@gmail.com>
2018-06-21 16:39:07 -0400

    removed classes

--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.javacohort3.ZipCodeBank.repositories;
-
-import com.example.javacohort3.ZipCodeBank.domains.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<Account, Long> {
-    Account findAccountById(Long id);
-    void deleteAccountById(Long id);
-}
--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.javacohort3.ZipCodeBank.repositories;
-
-import com.example.javacohort3.ZipCodeBank.domains.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    Customer findCustomerById(Long id);
-    void deleteCustomerById(Long id);
-}
