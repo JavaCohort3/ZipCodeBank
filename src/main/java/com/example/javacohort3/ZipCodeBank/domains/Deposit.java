c900ea19ecdedc55ed270beb0ca0cb3532f66aa1
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-22 13:50:44 -0400

    added @Entity

--- src/main/java/com/example/javacohort3/ZipCodeBank/domains/Deposit.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/domains/Deposit.java
@@ -1,9 +1,11 @@
 package com.example.javacohort3.ZipCodeBank.domains;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Deposit {
 
     @Id
