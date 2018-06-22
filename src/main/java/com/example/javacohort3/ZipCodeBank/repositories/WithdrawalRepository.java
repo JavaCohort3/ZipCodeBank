4db57fce027a88b6fd511bccf6df2c28235259ed
zeeman0306 <zeeman0306@gmail.com>
2018-06-22 14:45:29 -0400

    second commit

--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/WithdrawalRepository.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/WithdrawalRepository.java
@@ -1,4 +1,15 @@
 package com.example.javacohort3.ZipCodeBank.repositories;
 
-public interface WIthdrawalRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface WithdrawalRepository extends CrudRepository<Withdrawal, Long> {
+
+    Withdrawal createWithdrawalFromAccountId (Withdrawal withdrawal, Long accountId);
+    Withdrawal getWithdrawalById (Long id);
+    ArrayList<Withdrawal> getAllWithdrawalsForAccountId (Long accountId);
+    Withdrawal updateWithdrawal (Withdrawal withdrawal, Long id);
+    void deleteWithdrawal (Long Id);
+
 }
