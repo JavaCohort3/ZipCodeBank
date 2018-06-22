a63c21d890fc19a9d0aa0414695f3b1484afecd3
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-22 14:41:37 -0400

    fixed repo issue

--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/DepositRepository.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/DepositRepository.java
@@ -1,10 +1,11 @@
 package com.example.javacohort3.ZipCodeBank.repositories;
 
+import com.example.javacohort3.ZipCodeBank.domains.Deposit;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
 public interface DepositRepository extends CrudRepository<Deposit, Long> {
-    Deposit createDepositByAccountId(Deposit, Long);
-    ArrayList<Deposit> getAllDepositsForAccountId(Long);
+    Deposit createDepositByAccountId(Long id);
+    ArrayList<Deposit> getAllDepositsForAccountId(Long id);
 }
