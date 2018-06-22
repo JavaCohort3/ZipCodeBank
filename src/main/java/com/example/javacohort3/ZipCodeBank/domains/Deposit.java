97ff2c51b8f9aa638962dc484742a0b4f9e4e01f
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-22 13:27:47 -0400

    created Deposit POJO

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/domains/Deposit.java
@@ -0,0 +1,121 @@
+package com.example.javacohort3.ZipCodeBank.domains;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Deposit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Enum type;
+    private String transaction_date;
+    private String status;
+    private Long payee_id;
+    private String medium;
+    private Double amount;
+    private String description;
+    private Long account_id;
+
+    public Deposit(){}
+
+    public Deposit(Long id, Enum type, String transaction_date, String status, Long payee_id, String medium, Double amount, String description, Long account_id) {
+        this.id = id;
+        this.type = type;
+        this.transaction_date = transaction_date;
+        this.status = status;
+        this.payee_id = payee_id;
+        this.medium = medium;
+        this.amount = amount;
+        this.description = description;
+        this.account_id = account_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Enum getType() {
+        return type;
+    }
+
+    public void setType(Enum type) {
+        this.type = type;
+    }
+
+    public String getTransaction_date() {
+        return transaction_date;
+    }
+
+    public void setTransaction_date(String transaction_date) {
+        this.transaction_date = transaction_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getPayee_id() {
+        return payee_id;
+    }
+
+    public void setPayee_id(Long payee_id) {
+        this.payee_id = payee_id;
+    }
+
+    public String getMedium() {
+        return medium;
+    }
+
+    public void setMedium(String medium) {
+        this.medium = medium;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(Long account_id) {
+        this.account_id = account_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Deposit{" +
+                "id=" + id +
+                ", type=" + type +
+                ", transaction_date='" + transaction_date + '\'' +
+                ", status='" + status + '\'' +
+                ", payee_id=" + payee_id +
+                ", medium='" + medium + '\'' +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", account_id=" + account_id +
+                '}';
+    }
+}
