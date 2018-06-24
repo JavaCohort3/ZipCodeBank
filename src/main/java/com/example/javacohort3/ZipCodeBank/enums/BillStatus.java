f162e11b82a2c5c79edcc1cd1fa6fe38f456758b
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-24 19:41:23 -0400

    Created BillStatus Enum

---
+++ src/main/java/com/example/javacohort3/ZipCodeBank/enums/BillStatus.java
@@ -0,0 +1,34 @@
+package com.example.javacohort3.ZipCodeBank.enums;
+
+public enum BillStatus {
+    PENDING("PENDING"),
+    CANCELLED("CANCELLED"),
+    COMPLETED("COMPLETED"),
+    RECURRING("RECCURRING");
+
+    private final String value;
+
+    BillStatus(String value) {
+        this.value = value;
+    }
+
+    public static BillStatus fromValue(String value) {
+        if (value != null) {
+            for (BillStatus billStatus : values()) {
+                if (billStatus.value.equals(value)) {
+                    return billStatus;
+                }
+            }
+
+        }
+        return getDefault();
+    }
+
+    public String toValue() {
+        return value;
+    }
+
+    public static BillStatus getDefault() {
+        return PENDING;
+    }
+}
