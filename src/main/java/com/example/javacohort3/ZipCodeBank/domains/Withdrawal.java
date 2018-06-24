a707c81d23bdae425b0d9a4138a70f76a7a7bb9e
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-24 11:29:19 -0400

    changed the object for Type

--- src/main/java/com/example/javacohort3/ZipCodeBank/domains/Withdrawal.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/domains/Withdrawal.java
@@ -1,5 +1,7 @@
 package com.example.javacohort3.ZipCodeBank.domains;
 
+import com.example.javacohort3.ZipCodeBank.enums.TransactionType;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,7 +13,7 @@ public class Withdrawal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String type;
+    private TransactionType type;
     private String transactionDate;
     private String status;
     private Long payerId;
@@ -22,7 +24,7 @@ public class Withdrawal {
 
     public Withdrawal(){}
 
-    public Withdrawal(Long id, String type, String transactionDate, String status, Long payerId, String medium, Double amount, String description, Long accountId) {
+    public Withdrawal(Long id, TransactionType type, String transactionDate, String status, Long payerId, String medium, Double amount, String description, Long accountId) {
         this.id = id;
         this.type = type;
         this.transactionDate = transactionDate;
@@ -37,8 +39,8 @@ public class Withdrawal {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public Enum getType() { return type; }
-    public void setType(Enum type) { this.type = type; }
+    public TransactionType getType() { return type; }
+    public void setType(TransactionType type) { this.type = type; }
 
     public String getTransactionDate() { return transactionDate; }
     public void setTransactionDate(String transactionDate) { this.transactionDate = transactionDate; }
