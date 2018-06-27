86d80f1ef586f1360ab56095ce5b532f92d8283b
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-27 13:27:20 -0400

    Fixed BillController

--- src/main/java/com/example/javacohort3/ZipCodeBank/controllers/BillController.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/controllers/BillController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BillController {
@@ -49,7 +50,7 @@ public class BillController {
 
     @RequestMapping("/customers/{customerId}/bills")
     public ResponseEntity<?> getBillsByCustomerId(@PathVariable Long customerId){
-        Bill bill = billService.getBillsByCustomerId(customerId);
+        List<Bill> bill = billService.getBillsByCustomerId(customerId);
 
 
         log.info("[GET BY CUSTOMER ID]" + bill);
