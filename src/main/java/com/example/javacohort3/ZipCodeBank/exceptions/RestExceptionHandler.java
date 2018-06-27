9c1391023abee9ffde4a802567590ec9256df8e4
imowbray97 <isaiah.mowbray97@gmail.com>
2018-06-26 21:38:43 -0400

    Fix everything bill related

--- src/main/java/com/example/javacohort3/ZipCodeBank/exceptions/RestExceptionHandler.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/exceptions/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.javacohort3.ZipCodeBank.exceptions;
 
 
+import com.fasterxml.jackson.core.JsonParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,11 @@ public class RestExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleAccountNotFoundException(ResourceNotFoundException rnfe){
         return new ResponseEntity<>(new ErrorDetails(rnfe.getStatus(),rnfe.getMessage()),rnfe.getStatus());
+    }
+
+    @ExceptionHandler(JsonParseException.class)
+    public ResponseEntity<?> handleJsonParseException(JsonParseException jpe){
+        return new ResponseEntity<>(new ErrorDetails(HttpStatus.BAD_REQUEST,"|| Java Cohort 3 || " + jpe.getMessage()),HttpStatus.BAD_REQUEST);
     }
 
 }
