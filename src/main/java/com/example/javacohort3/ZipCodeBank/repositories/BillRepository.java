c5d95caf09e964416e47869fa1364c9599a8ebe2
Jermaine34343 <Jermaine34343@gmail.com>
2018-06-26 20:27:16 -0400

    added Bill POJO imports

--- src/main/java/com/example/javacohort3/ZipCodeBank/repositories/BillRepository.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/repositories/BillRepository.java
@@ -1,8 +1,8 @@
 package com.example.javacohort3.ZipCodeBank.repositories;
 
+import com.example.javacohort3.ZipCodeBank.domains.Bill;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
 
 public interface BillRepository extends CrudRepository<Bill, Long> {
 
