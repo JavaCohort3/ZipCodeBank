0d9ea9ec4231700a1582233b440a81ddf1383927
imowbray97 <isaiah.mowbray97@gmail.com>
2018-06-27 17:49:15 -0400

    RestHandler handles Multiples enity

--- src/main/java/com/example/javacohort3/ZipCodeBank/exceptions/RestExceptionHandler.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/exceptions/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.javacohort3.ZipCodeBank.exceptions;
 
 
+import org.hibernate.PersistentObjectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,4 +14,13 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(new ErrorDetails(rnfe.getStatus(),rnfe.getMessage()),rnfe.getStatus());
     }
 
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> handleIllegalStateException(IllegalStateException ise){
+        return new ResponseEntity<>(new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR,"Can not have the same entity more than one "+ " :  " + "Follow us on Github | - JavaCohort | Zay, Manny, Blaude, Jermaine, Zamir , & JoJo"),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(PersistentObjectException.class)
+    public ResponseEntity<?> handlePersistentObjectException(PersistentObjectException poe){
+        return new ResponseEntity<>(new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR,poe.getLocalizedMessage() + "   " + "Follow us on Github | - JavaCohort | Zay, Manny, Blaude, Jermaine, Zamir , & JoJo"),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
