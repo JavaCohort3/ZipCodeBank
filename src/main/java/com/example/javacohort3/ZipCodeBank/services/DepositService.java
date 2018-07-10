b0edf3b116e3838677d2d1f0c517d0c826a01dc4
zeeman0306 <zeeman0306@gmail.com>
2018-07-10 16:22:28 -0400

    made changes to Deposit Controller and Service

--- src/main/java/com/example/javacohort3/ZipCodeBank/services/DepositService.java
+++ src/main/java/com/example/javacohort3/ZipCodeBank/services/DepositService.java
@@ -4,6 +4,7 @@ import com.example.javacohort3.ZipCodeBank.domains.Deposit;
 import com.example.javacohort3.ZipCodeBank.exceptions.ResourceNotFoundException;
 import com.example.javacohort3.ZipCodeBank.repositories.AccountRepository;
 import com.example.javacohort3.ZipCodeBank.repositories.DepositRepository;
+import com.example.javacohort3.ZipCodeBank.util.Burn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,7 @@ public class DepositService {
         this.accountRepository = accountRepository;
     }
 
+    // Verify
     public void verifyDepositById(Long id){
         if (depositRepository.findDepositById(id) == null) throw new ResourceNotFoundException(HttpStatus.NOT_FOUND," error fetching deposit with id ");
     }
@@ -30,10 +32,13 @@ public class DepositService {
         if (accountRepository.findAccountById(id) == null) throw new ResourceNotFoundException(HttpStatus.NOT_FOUND,"Account Not Found");
     }
 
-    public Deposit createDeposit(Deposit deposit){
+    // Create
+    public Deposit createDeposit(Deposit deposit, Long accountId){
+        verifyDepositById(accountId);
         return depositRepository.save(deposit);
     }
 
+    // Get
     public List<Deposit> getDepositsByAccountId(Long accountId) {
         verifyAccountById(accountId);
 
@@ -43,15 +48,31 @@ public class DepositService {
         return deposits;
     }
 
+    // Get By Account ID
     public Deposit getDepositById (Long id){
+        verifyDepositById(id);
         return depositRepository.findDepositById(id);
     }
 
+    // Update
     public Deposit updateDeposit(Deposit deposit){
+
+        // verifies deposit before proceeding
+        verifyDepositById(deposit.getId());
+
+        // sets old deposit to variable
+        Deposit oldDeposit = getDepositById(deposit.getId());
+
+        // casts Burn.updateObjectFields return value to Deposit
+        deposit = (Deposit) Burn.updateObjectFields(deposit, oldDeposit);
+
+        // saves and returns customer
         return depositRepository.save(deposit);
     }
 
+    // Delete
     public void deleteDepositById(Long id){
+        verifyDepositById(id);
         depositRepository.deleteById(id);
     }
 }
